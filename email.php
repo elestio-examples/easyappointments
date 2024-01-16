0dd60b3aca5f25001b6ac429a48fdc7b5b142706
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2024-01-16 15:46:43 +0530

    Update email.php

--- email.php
+++ email.php
@@ -1,13 +1,16 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-// Add custom values by settings them to the $config array.
+// Add custom values by setting them to the $config array.
 // Example: $config['smtp_host'] = 'smtp.gmail.com';
 // @link https://codeigniter.com/user_guide/libraries/email.html
 
 $config['useragent'] = 'Easy!Appointments';
 $config['protocol'] = 'smtp';
 $config['mailtype'] = 'html';
-$config['smtp_debug'] = '1';
+$config['smtp_debug'] = '0';
 $config['smtp_auth'] = FALSE;
 $config['smtp_host'] = '172.17.0.1';
 $config['smtp_port'] = 25;
+$config['smtp_user'] = "";
+$config['smtp_pass'] = "";
+$config['smtp_crypto'] = "";
